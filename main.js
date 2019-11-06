89719658db3130fbc6f9ab1aaae5fdc4b0c9a07c
Colton Steeve <coltonsteeve@gmail.com>
2019-11-05 20:57:19 -0600

    Added Hex Code and Links

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+var color = {};
+
+//Convert RGB to Hex
+var rgbToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+//Get full hex string
+var fullColorHex = function(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
+};
+
 // define handler
 const updateColor = () => {
-  const r = parseInt(document.querySelector('#red').value)
-  const g = parseInt(document.querySelector('#green').value)
-  const b = parseInt(document.querySelector('#blue').value)
+  r = parseInt(document.querySelector('#red').value)
+  g = parseInt(document.querySelector('#green').value)
+  b = parseInt(document.querySelector('#blue').value)
   const canvasElement = document.querySelector('#canvas')
   canvasElement.style.backgroundColor = `rgb(${r},${g},${b})`
   canvasElement.style.border = "thin solid black"
+  document.querySelector('#hex').innerHTML = "#" + fullColorHex(r,g,b)
 }
+
 
 // configure event listeners
 document.querySelector('#red').addEventListener('click', updateColor)
